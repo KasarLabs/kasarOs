d0b0142b8d79e9338dcd4f0c23ea3b640001bc25
0xTrinityy <117627242+0xTrinityy@users.noreply.github.com>
2023-03-08 17:35:23 +0000

    Modifying reporting.rs to get hostname

--- src/nodes/reporting.rs
+++ src/nodes/reporting.rs
@@ -1,9 +1,29 @@
 use super::config::Config;
+use std::thread;
+use std::collections::HashMap;
 
-pub fn enable_metrics(conf: &Config) {
-    if !conf.metrics.enabled {
+pub fn enable_metrics(conf: &Config) 
+{
+    if !conf.metrics.enabled 
+    {
         return;
     }
+    metrics.enabled = true;
+    //this function just check if we have a hostname env
+    let host = match env::hostname() 
+    {
+        Ok(val) => val,
+        Err(e) => panic!("No hostname env , error : {:?}", e),
+    }
+    //tag insertion using hashmap to find host  
+    host = host.to_string();
+    let tags: HashMap<String, String> = [("host".to_string(), host)].iter().cloned().collect();
 
-    //TO DO
+    log.Info("Starting metrics", "url", conf.Metrics.Endpoint,
+        "db", conf.Metrics.Database, "namespace", conf.Metrics.Namespace);
+    let influxtag = thread::spawn(||
+    { registry, 10*time.Second,conf.Metrics.Endpoint, conf.Metrics.Database,
+        conf.Metrics.Username, conf.Metrics.Password, conf.Metrics.Namespace, tags
+    })
+    influxtag.join().unwrap();
 }
