2fe65b60980d0c501268b12569949fd24f3ba6dc
antiyro <antiyro@gmail.com>
2023-03-14 16:25:34 +0100

    corrected menu

--- system/install.sh
+++ system/install.sh
@@ -6,36 +6,18 @@ test $? -eq 0 || exit 1 "You should have sudo privilege to run this script."
 
 source ./utils.sh
 
-isOsiris
+# Main menu
+main() {
+    osirisClear
+    options=("Payrus - Starkware" "Juno - Nethermind" "Pathfinder - Equilibrium" "Quit")
+    yesOrNo=("Yes" "No" "Quit")
+  	if [ -f "/usr/local/bin/osiris" ]
+	then
+		print_menu "Hi again 👋 myOsiris already installed!" "Do you want to install another client?" "${yesOrNo[@]}"
+	else
+		print_menu "Welcome to myOsiris! 👋" "Please chose the client you'd like to install" "${options[@]}"
+	fi
 
-echo -e "\n\033[34mInstalling tools pre-requisites... \033[m\n"
-	sleep 1
-	while read -r p ; do sudo apt install -y $p ; done < <(cat << "EOF"
-	build-essential
-	libncurses5-dev 
-	libpcap-dev
-	git
-	jq
-EOF
-)
+}
 
-echo -e "\n\033[34mInstalling tools... \033[m\n"
-	sleep 1
-	while read -r p ; do sudo apt install -y $p ; done < <(cat << "EOF"
-	sysstat
-	bc
-EOF
-)
-
-if [ ! -d "$(pwd)/tmp/" ]
-then
-	mkdir $(pwd)/tmp/
-fi
-
-
-git -C $(pwd)/tmp/ clone https://github.com/raboof/nethogs >& $(pwd)/tmp/sample.log
-sudo make install -C $(pwd)/tmp/nethogs/ >& $(pwd)/tmp/sample.log
-rm -rf $(pwd)/tmp/
-sudo cp $(pwd)/osiris /usr/local/bin
-
-echo -e "\n\033[1;32mDone !\033[m\n"
+main
--- system/utils.sh
+++ system/utils.sh
@@ -33,19 +33,6 @@ osirisClear() {
 # Define options and default selected option
 selected=0
 
-# Check if Osiris is already installed
-
-isOsiris() {
-    osirisClear
-    options=("Payrus - Starkware" "Juno - Nethermind" "Pathfinder - Equilibrium" "Quit")
-  	if [ -f "/usr/local/bin/osiris" ]
-	then
-		print_menu "myOsiris already installed !" "Do you want to install another client?" "${options[@]}"
-	else
-		echo -e "\nPlease choose the client you'd like to install:\n"
-	fi
-}
-
 # Function to print the menu
 print_menu() {
     osirisClear
@@ -87,10 +74,44 @@ print_menu() {
                     echo "See you soon!"
                 else
                     echo "You selected ${options[$selected]}"
-                    choice = ${options[$selected]}
                 fi
-                exit 0
+                break
                 ;;
         esac
     done
+    return ${selected}
+}
+
+installTools() {
+	echo -e "\n\033[34mInstalling tools pre-requisites... \033[m\n"
+		sleep 1
+		while read -r p ; do sudo apt install -y $p ; done < <(cat << "EOF"
+		build-essential
+		libncurses5-dev 
+		libpcap-dev
+		git
+		jq
+EOF
+)
+
+	echo -e "\n\033[34mInstalling tools... \033[m\n"
+		sleep 1
+		while read -r p ; do sudo apt install -y $p ; done < <(cat << "EOF"
+		sysstat
+		bc
+EOF
+)
+
+	if [ ! -d "$(pwd)/tmp/" ]
+	then
+		mkdir $(pwd)/tmp/
+	fi
+
+
+	git -C $(pwd)/tmp/ clone https://github.com/raboof/nethogs >& $(pwd)/tmp/sample.log
+	sudo make install -C $(pwd)/tmp/nethogs/ >& $(pwd)/tmp/sample.log
+	rm -rf $(pwd)/tmp/
+	sudo cp $(pwd)/osiris /usr/local/bin
+
+	echo -e "\n\033[1;32mDone !\033[m\n"
 }
