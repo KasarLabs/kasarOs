a50972358674d871dbb8c2ada21ba3b3f11eb0a0
antiyro <antiyro@gmail.com>
2023-03-16 16:52:00 +0100

    added setSyncTime

--- network/l2/scanner/scanner.go
+++ network/l2/scanner/scanner.go
@@ -21,8 +21,48 @@ const (
 
 type Block struct {
 	number    int64
-	syncTime  int64
-	hash      string
+	timestamp  int64
+	last_ts	int64
+}
+
+var block = Block {
+	number: 0,
+	timestamp: 0,
+	last_ts: 0,
+}
+
+type SyncTime struct {
+	last	int64
+	min		int64
+	max 	int64
+	avg		int64
+	count	int64
+}
+
+var syncTime = SyncTime {
+	last: 0,
+	min: 0,
+	max: 0,
+	avg: 0,
+	count: 0,
+}
+
+func setSyncTime(block Block) {
+	syncTime.count += 1;
+	syncTime.last = block.timestamp - block.last_ts;
+	if (syncTime.count > 1) {
+		// Set min max syncTime
+		if (syncTime.last > syncTime.max) {
+			syncTime.max = syncTime.last
+		} else if (syncTime.last < syncTime.min) {
+			syncTime.min = syncTime.last
+		}
+		// Set avg syncTime
+		syncTime.avg = (syncTime.avg + syncTime.last) / 2
+	} else {
+		syncTime.min = syncTime.last
+	}
+	fmt.Println(syncTime)
 }
 
 func Scanner() {
@@ -30,7 +70,9 @@ func Scanner() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	if (syncTime.last == 0) {
+		syncTime.last = time.Now().Unix()
+	}
 	// Loop continuously to read new lines from the file
 	for {
 		file, err := os.Open(absPath)
@@ -42,19 +84,19 @@ func Scanner() {
 			line := scanner.Text()
 			if len(line) > 0 {
 				liner := strings.ReplaceAll(utils.RemoveBraces(line), " ", "\t")
-				lineParsed := strings.Split(liner, "\t")
-				timestamp, err := utils.ParseTimestamp(lineParsed[0] + " " + lineParsed[1] + " " + lineParsed[2])
-				if err != nil {
-					fmt.Println(err)
-				}
-				syncTime := timestamp.Unix()
 				number, _ := strconv.ParseInt(utils.ExtractNumber(liner), 10, 64)
-				block := Block{
-					number:   number,
-					syncTime: syncTime,
+				if (number > block.number) {
+					lineParsed := strings.Split(liner, "\t")
+					time, err := utils.ParseTimestamp(lineParsed[0] + " " + lineParsed[1] + " " + lineParsed[2])
+					if err != nil {
+						fmt.Println(err)
+					}
+					timestamp := time.Unix()
+					block.number = number
+					block.last_ts = block.timestamp
+					block.timestamp = timestamp
+					setSyncTime(block)
 				}
-				fmt.Println("block", block)
-				// storeBlock(db, block)
 			}
 		}
 		if err := scanner.Err(); err != nil {
