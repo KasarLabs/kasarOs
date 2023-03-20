fa0a0138df58b6816af5614a9dd980e7c401d832
antiyro <antiyro@gmail.com>
2023-03-20 19:16:03 +0100

    cleaned up calls

---
+++ db/db.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"log"
+	"fmt"
+    "net/http"
+	"encoding/json"
+	"myOsiris/network/config"
+)
+
+const Url = "http://localhost:8080/"
+var nodeConfig config.Config = config.User
+
+func NodeExist() (bool) {
+	resp, err := http.Get(Url + "nodeExists?nodeID=" + nodeConfig.OsirisKey)
+    if err != nil {
+        log.Fatalf("Error making HTTP request: %v", err)
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        log.Fatalf("Received non-OK status code: %d", resp.StatusCode)
+    }
+
+    var response struct {
+        Exists bool `json:"exists"`
+    }
+    err = json.NewDecoder(resp.Body).Decode(&response)
+    if err != nil {
+        log.Fatalf("Error decoding JSON response: %v", err)
+    }
+
+    if response.Exists {
+        fmt.Printf("Node %s exists\n", nodeConfig.OsirisKey)
+		return(true)
+	}
+    fmt.Printf("Node %s does not exist\n", nodeConfig.OsirisKey)
+	return(false)
+}
--- main.go
+++ main.go
@@ -6,40 +6,11 @@ import (
 	// "myOsiris/network/scannerL2"
     _ "github.com/lib/pq" // Import PostgreSQL driver
 	// "myOsiris/system"
-	"log"
-	"fmt"
-    "net/http"
-	"encoding/json"
-	"myOsiris/network/config"
+	"myOsiris/db"
 )
 
 func main() {
-	nodeConfig := config.User
-
-    url := fmt.Sprintf("http://localhost:8080/nodeExists?nodeID=%s", nodeConfig.OsirisKey)
-    resp, err := http.Get(url)
-    if err != nil {
-        log.Fatalf("Error making HTTP request: %v", err)
-    }
-    defer resp.Body.Close()
-
-    if resp.StatusCode != http.StatusOK {
-        log.Fatalf("Received non-OK status code: %d", resp.StatusCode)
-    }
-
-    var response struct {
-        Exists bool `json:"exists"`
-    }
-    err = json.NewDecoder(resp.Body).Decode(&response)
-    if err != nil {
-        log.Fatalf("Error decoding JSON response: %v", err)
-    }
-
-    if response.Exists {
-        fmt.Printf("Node %s exists\n", nodeConfig.OsirisKey)
-    } else {
-        fmt.Printf("Node %s does not exist\n", nodeConfig.OsirisKey)
-    }
+    db.NodeExist()
 	// go func() {
 	// 	for {
 	// 		scannerL2.ScannerL2()
