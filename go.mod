a42fcf2ab1ea9a625afa29e912eccce6d9ba163b
antiyro <antiyro@gmail.com>
2023-03-20 17:34:23 +0100

    adding routes testing

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module myOsiris
 go 1.20
 
 require (
+	github.com/99designs/gqlgen v0.17.26 // indirect
 	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
@@ -12,12 +14,19 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/graphql-go/graphql v0.8.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.1 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/shirou/gopsutil/v3 v3.23.2 // indirect
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
 	github.com/tklauser/numcpus v0.6.0 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.1 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
--- main.go
+++ main.go
@@ -4,16 +4,42 @@ import (
 
 	// "myOsiris/network/scannerL1"
 	// "myOsiris/network/scannerL2"
-	"myOsiris/system"
-)
-
-
-const (
-	logsFile           = "network/logs.txt"
-	dbConnectionString = "root:tokenApi!@tcp(localhost:3306)/juno"
+    _ "github.com/lib/pq" // Import PostgreSQL driver
+	// "myOsiris/system"
+	"log"
+	"fmt"
+    "net/http"
+	"encoding/json"
+	"myOsiris/network/config"
 )
 
 func main() {
+	nodeConfig := config.User
+
+    url := fmt.Sprintf("http://localhost:8080/nodeExists?nodeID=%s", nodeConfig.OsirisKey)
+    resp, err := http.Get(url)
+    if err != nil {
+        log.Fatalf("Error making HTTP request: %v", err)
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        log.Fatalf("Received non-OK status code: %d", resp.StatusCode)
+    }
+
+    var response struct {
+        Exists bool `json:"exists"`
+    }
+    err = json.NewDecoder(resp.Body).Decode(&response)
+    if err != nil {
+        log.Fatalf("Error decoding JSON response: %v", err)
+    }
+
+    if response.Exists {
+        fmt.Printf("Node %s exists\n", nodeConfig.OsirisKey)
+    } else {
+        fmt.Printf("Node %s does not exist\n", nodeConfig.OsirisKey)
+    }
 	// go func() {
 	// 	for {
 	// 		scannerL2.ScannerL2()
@@ -25,10 +51,10 @@ func main() {
 	// 		scannerL1.ScannerL1()
 	// 	}	
 	// }()
-	go func() {
-		for {
-			system.ScannerSystem()
-		}
-	}()
-	select {}
+	// go func() {
+	// 	for {
+	// 		system.ScannerSystem()
+	// 	}
+	// }()
+	// select {}
 }
