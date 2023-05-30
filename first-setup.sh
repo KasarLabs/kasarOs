1e4da646c8ec3c1ec8768369434a6f95e6f2eb74
HxSimo <HxSimo@proton.me>
2023-05-30 04:27:50 +0200

    install script

--- first-setup.sh
+++ first-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # sudo apt update 
-# sudo apt install git
-# git clone https://github.com/KasarLabs/kasarOs
-# cd kasarOs
-# git checkout roc-dev
+# sudo apt install git -y
+# git clone https://github.com/KasarLabs/kasarOs && cd kasarOs && git checkout roc-dev
+
+git config pull.ff only
+sudo su -
+cd /home/starknode/kasarOs/
+git config --global --add safe.directory /home/starknode/kasarOs
+mv rc.local /etc/rc.local
