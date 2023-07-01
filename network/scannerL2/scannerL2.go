25306b87257909decdbe208337ee55088e638f84
HxSimo <HxSimo@proton.me>
2023-07-01 22:44:15 +0200

    L2 sync work

--- network/scannerL2/scannerL2.go
+++ network/scannerL2/scannerL2.go
@@ -72,62 +72,57 @@ func ScannerL2(baseUrl string, nodeId uint) types.L2 {
 			panic(err)
 		}
 		scanner := bufio.NewScanner(file)
+		var lastLine string
 		for scanner.Scan() {
-			line := strings.ReplaceAll(utils.RemoveBraces(scanner.Text()), " ", "\t")
-			if len(line) > 0 {
-				number, _ := strconv.ParseInt(utils.ExtractNumber(line), 10, 64)
-				if (number > local.Number && number < local.Number+500) || (number < local.Number) {
-					local.Number = number
-					local.Prev_timestamp = local.Timestamp
-					local.Timestamp, _ = utils.ExtractTimestamp(line)
-					syncTime := getSyncTime(local)
-					if syncTime.Last.Seconds() > 9999999 {
-						continue
-					}
-
-					/*rows, err := db.Query("INSERT INTO l2 (node_id, block_id, sync_time) VALUES ($1, $2, $3)", nodeId, l2.Block.Number, l2.SyncTime.Avg.Seconds())
-					if err != nil {
-						log.Fatal(err)
-					}
-					defer rows.Close()*/
-					//fmt.Printf("\033[s\033[2K\rL2 - Block number %d with id %s synced in %.2f secs - avg sync time %.2f \033[u", l2.Block.Number, utils.FormatHash(l2.Block.Hash), l2.SyncTime.Last.Seconds(), l2.SyncTime.Avg.Seconds())
-					data := L2{
-						NodeID:   nodeId,
-						Block:    uint(local.Number),
-						SyncTime: syncTime.Last.Seconds(),
-					}
-					jsonData, err := json.Marshal(data)
-					if err != nil {
-						fmt.Println("Error L2:", err)
-						return types.L2{}
-					}
-					request, err := http.NewRequest("POST", baseUrl, bytes.NewBuffer(jsonData))
-					if err != nil {
-						fmt.Println("Error L2:", err)
-						return types.L2{}
-					}
-					request.Header.Set("Content-Type", "application/json")
-
-					client := &http.Client{}
-					response, err := client.Do(request)
-					if err != nil {
-						fmt.Println("Error L2:", err)
-						return types.L2{}
-					}
-					response.Body.Close()
+			lastLine = scanner.Text()
+		}
+		line := strings.ReplaceAll(utils.RemoveBraces(lastLine), " ", "\t")
+		if len(line) > 0 {
+			number, _ := strconv.ParseInt(utils.ExtractNumber(line), 10, 64)
+			if (number > local.Number && number < local.Number+500) || (number < local.Number) || (local.Number == 0) {
+				local.Number = number
+				local.Prev_timestamp = local.Timestamp
+				local.Timestamp, _ = utils.ExtractTimestamp(line)
+				syncTime := getSyncTime(local)
+				if syncTime.Last.Seconds() > 9999999 {
 					continue
 				}
+				
+				data := L2{
+					NodeID:   nodeId,
+					Block:    uint(local.Number),
+					SyncTime: syncTime.Last.Seconds(),
+				}
+				jsonData, err := json.Marshal(data)
+				if err != nil {
+					fmt.Println("Error L2:", err)
+					return types.L2{}
+				}
+				request, err := http.NewRequest("POST", baseUrl, bytes.NewBuffer(jsonData))
+				if err != nil {
+					fmt.Println("Error L2:", err)
+					return types.L2{}
+				}
+				request.Header.Set("Content-Type", "application/json")
+				client := &http.Client{}
+				response, err := client.Do(request)
+				if err != nil {
+					fmt.Println("Error L2:", err)
+					return types.L2{}
+				}
+				response.Body.Close()
+				continue
 			}
-			// Update the size variable to the current byte offset
-			size, err = file.Seek(0, os.SEEK_CUR)
-			if err != nil {
-				panic(err)
-			}
+		}
+		// Update the size variable to the current byte offset
+		size, err = file.Seek(0, os.SEEK_CUR)
+		if err != nil {
+			panic(err)
 		}
 		if err := scanner.Err(); err != nil {
 			fmt.Println("Error scanning file:", err)
 		}
 		file.Close()
-		time.Sleep(1 * time.Second) // wait for 1 second before checking the file again
+		time.Sleep(10 * time.Second) // wait for 1 second before checking the file again
 	}
 }
