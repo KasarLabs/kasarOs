96275b4316d71d7dda1f9c8300a6e92413a7ad6e
HxSimo <HxSimo@proton.me>
2023-07-03 13:31:16 +0200

    Shutdown and Reboot

--- verify.sh
+++ verify.sh
@@ -7,6 +7,20 @@ CLIENT_DIR="$KASAROS_PATH/client"
 LOGS_PATH="$KASAROS_PATH/network/logs.txt"
 CONFIG_PATH="/boot/efi/src/config.json"
 client=$(jq -r '.client' $CONFIG_PATH)
+provider_id=$(jq -r '.provider_id' $CONFIG_PATH)
+node_id=$(jq -r '.node_id' $CONFIG_PATH)
+action=""
+
+getAction() {
+    URL="http://179.61.246.59:8080/node/getAction?provider_id=$provider_id"
+    DATA="{
+        \"NodeID\": $node_id,
+        \"Action\": \"\",
+        \"Command\": \"\",
+        \"Response\": \"\"
+    }"
+    action=curl -X POST -H "Content-Type: application/json" -d "$DATA" "$URL"
+}
 
 check_script() {
     if ! pgrep -f prog_name >/dev/null; then
@@ -31,8 +45,14 @@ while true; do
     elif pgrep -f "myOsiris" >/dev/null; then
         step=3
     else
-        sleep 20
+        sleep 5
         check_script
+    fi
+    getAction
+    if [ "$action" = "shutdown" ]; then
+        sudo poweroff
+    elif [ "$action" = "reboot" ]; then
+        sudo reboot
     fi
     sleep 20
 done
