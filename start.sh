8d76ec6cf098d0e749bbdee00ca25bc8d4cfec36
HxSimo <HxSimo@proton.me>
2023-07-10 12:02:19 +0200

    Gpg error patch

--- start.sh
+++ start.sh
@@ -188,7 +188,7 @@ EOF
             curl \
             gnupg \
             lsb-release
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo "" | sudo tee /etc/apt/sources.list.d/docker.list
         sudo apt-get update
         sudo apt-get install -y docker.io
